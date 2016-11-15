f4c4c420c9e4e956b8de4617a9a2183c78995f68
David Aghassi <david.aghassi@gmail.com>
2016-11-15 15:30:30 -0500

    Fixed syntax errors

--- scripts/create_database.js
+++ scripts/create_database.js
@@ -7,59 +7,59 @@ var dbconfig = require('../config/database');
 
 var connection = mysql.createConnection(dbconfig.connection);
 
-connection.query('CREATE DATABASE ' + dbconfig.database);
+connection.query('CREATE SCHEME IF NOT EXISTS ' + dbconfig.database);
 
 /** Tables **/
 
 // Users
 connection.query('\
-CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.users_table + '` ( \
+CREATE TABLE IF NOT EXISTS`' + dbconfig.database + '`.`' + dbconfig.users_table + '` ( \
   `user_id` INT NOT NULL AUTO_INCREMENT, \
   `name` NVARCHAR(20) NULL, \
   `title` NVARCHAR(20) NULL, \
   `dept` NVARCHAR(20) NULL, \
   `username` NVARCHAR(45) NULL, \
   `password` NVARCHAR(20) NULL, \
-  PRIMARY KEY (`user_id`), \
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC)) \
+      PRIMARY KEY (`user_id`), \
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) \
 )');
 
 // Managers
 connection.query('\
-CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.managers_table + '` ( \
+CREATE TABLE IF NOT EXISTS`' + dbconfig.database + '`.`' + dbconfig.managers_table + '` ( \
   `dept` NVARCHAR(20) NOT NULL, \
   `user_id` INT NOT NULL, \
   PRIMARY KEY (`user_id`), \
   CONSTRAINT `user_id` \
     FOREIGN KEY (`user_id`) \
-    REFERENCES `eecs341`.`users` (`user_id`) \
+    REFERENCES `' + dbconfig.database + '`.`' + dbconfig.users_table + '` (`user_id`) \
     ON DELETE CASCADE \
-    ON UPDATE CASCADE) \
+    ON UPDATE CASCADE \
 )');
 
 // Events
 connection.query('\
-CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.events_table + '` ( \
+CREATE TABLE IF NOT EXISTS`' + dbconfig.database + '`.`' + dbconfig.events_table + '` ( \
   `event_id` INT NOT NULL AUTO_INCREMENT, \
   `creator` INT NOT NULL, \
   `start_time` DATETIME NOT NULL, \
   `end_time` DATETIME NOT NULL, \
   `description` NVARCHAR(100) NULL, \
   `created_date` DATETIME NOT NULL, \
-  PRIMARY KEY (`event_id`)) \
+  PRIMARY KEY (`event_id`) \
 )');
 
 // Manages
 connection.query('\
-CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.manages_table + '` ( \
+CREATE TABLE IF NOT EXISTS`' + dbconfig.database + '`.`' + dbconfig.manages_table + '` ( \
   `manager_id` INT NOT NULL, \
   `user_id` INT NOT NULL, \
   PRIMARY KEY (`manager_id`), \
   CONSTRAINT `manager_id` \
     FOREIGN KEY (`manager_id`) \
-    REFERENCES `eecs341`.`users` (`user_id`) \
+    REFERENCES `' + dbconfig.database + '`.`' + dbconfig.users_table + '` (`user_id`) \
     ON DELETE CASCADE \
-    ON UPDATE CASCADE) \
+    ON UPDATE CASCADE \
 )');
 
 // Schedules
@@ -71,9 +71,9 @@ CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.schedules_table + '` ( \
   INDEX `user_id_idx` (`employee_id` ASC), \
   CONSTRAINT `employee_id` \
     FOREIGN KEY (`employee_id`) \
-    REFERENCES `eecs341`.`users` (`user_id`) \
+    REFERENCES `' + dbconfig.database + '`.`' + dbconfig.users_table + '` (`user_id`) \
     ON DELETE NO ACTION \
-    ON UPDATE NO ACTION) \
+    ON UPDATE NO ACTION \
 )');
 
 // Invites
@@ -86,9 +86,9 @@ CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.invites_table + '` ( \
   INDEX `user_id_idx` (`employee` ASC), \
   CONSTRAINT `employee` \
     FOREIGN KEY (`employee`) \
-    REFERENCES `eecs341`.`users` (`user_id`) \
+    REFERENCES `' + dbconfig.database + '`.`' + dbconfig.users_table + '` (`user_id`) \
     ON DELETE CASCADE \
-    ON UPDATE CASCADE) \
+    ON UPDATE CASCADE \
 )');
 
 console.log('Success: Database Created!')
