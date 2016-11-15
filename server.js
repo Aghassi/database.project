6ab58b169fbe2f3c004eda9d9ff7935c660238f7
David Aghassi <david.aghassi@gmail.com>
2016-11-14 21:25:40 -0500

    Added routes and view

--- server.js
+++ server.js
@@ -6,8 +6,29 @@ var connection = mysql.createConnection({
   password : 'databaseproject',
   database : 'eecs341'
 });
+var port         = process.env.PORT || 8080;
+var passport     = require('passport');
+var flash    = require('connect-flash');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
 var app = express();
 
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'eecs341databaseproject' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
 connection.connect(function(err){
   if(!err) {
     console.log("Database is connected ... \n\n");
@@ -20,4 +41,5 @@ app.get("/",function(req,res){
   /* end point, can create connection queries from within these and return*/
 });
 
-app.listen(3000);
+app.listen(port);
+console.log('The magic happens on port ' + port);
